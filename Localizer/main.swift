9fa3101764516ca20bf85c0e9d0280f380de4748
Frizlab <fload@me.com>
2014-09-25 11:18:52 +0200

    Initial Commit

---
+++ Localizer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Localizer
+//
+//  Created by François Lamboley on 9/25/14.
+//  Copyright (c) 2014 happn. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
