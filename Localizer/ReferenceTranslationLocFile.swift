34b659c95cfe3c722bc3352d1dc8b09b4d84b42b
Frizlab <fload@me.com>
2016-07-06 17:49:21 +0200

    Create ReferenceTranslationLocFile

---
+++ Localizer/ReferenceTranslationLocFile.swift
@@ -0,0 +1,50 @@
+/*
+ * ReferenceTranslationLocFile.swift
+ * Localizer
+ *
+ * Created by François Lamboley on 7/6/16.
+ * Copyright © 2016 happn. All rights reserved.
+ */
+
+import Foundation
+
+
+
+class ReferenceTranslationLocFile {
+	
+	typealias Key = String
+	typealias Value = String
+	typealias Language = String
+	
+	private(set) var languages: [Language]
+	private(set) var entries: [Key: [Language: Value]]
+	
+	convenience init(fromURL url: URL, languages: [Language], csvSeparator: String = ",") throws {
+		var encoding = String.Encoding.utf8
+		let filecontent = try String(contentsOf: url, usedEncoding: &encoding)
+		try self.init(filecontent: filecontent, languages: languages, csvSeparator: csvSeparator)
+	}
+	
+	init(filecontent: String, languages sourceLanguages: [Language], csvSeparator: String = ",") throws {
+		let error = NSError(domain: "ReferenceTranslationLocFile", code: 1, userInfo: nil)
+		let parser = CSVParser(source: filecontent, separator: csvSeparator, hasHeader: true, fieldNames: nil)
+		guard let parsedRows = parser.arrayOfParsedRows() else {
+			throw error
+		}
+		
+		var entriesBuilding = [Key: [Language: Value]]()
+		for row in parsedRows {
+			guard let key = row["KEY"] else {continue}
+			if entriesBuilding[key] != nil {
+				print("*** Warning: Found duplicated key \(key) when parsing reference translation loc file. The latest one wins.")
+			}
+			
+			var values = [Language: Value]()
+			for language in sourceLanguages {values[language] = row[language] ?? ""}
+			entriesBuilding[key] = values
+		}
+		languages = sourceLanguages
+		entries = entriesBuilding
+	}
+	
+}
