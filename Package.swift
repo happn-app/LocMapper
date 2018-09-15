1d275657cd1c88b5fdbb52b01aa970eb22eaa88a
Frizlab <fload@me.com>
2018-09-15 19:58:33 +0200

    Make the project SPM compatible (only for the CLI; prepares Linux compatibility)

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+
+let package = Package(
+	name: "LocMapper",
+	dependencies: [
+		.package(url: "git@github.com:happn-app/XibLoc.git", from: "0.9.2")
+	],
+	targets: [
+		.target(name: "LocMapper", dependencies: ["XibLoc"], path: "LocMapper"), /* A better name would be LocMapperKit. I’m lazy enough not to refactor. */
+		.target(name: "locmapper", dependencies: ["LocMapper"], path: "LocMapper CLI")
+		/* As an alternative to the two targets above, we can have only one
+		 * “locmapper” target that compile both folders directly. I prefer the
+		 * lib/executable structure (among other it allows having other targets
+		 * that uses the LocMapper lib), but here’s the alternative (note it does
+		 * not work out of the box because the FileHandleOutputStream class is
+		 * implemented both in the lib and the executable…):
+		.target(name: "locmapper", dependencies: ["XibLoc"], path: ".", sources: ["LocMapper", "LocMapper CLI"])*/
+	]
+)
