f9e904442036b1f1590dd436276ec87b1639bdcd
Frizlab <fload@me.com>
2018-12-12 14:32:03 +0100

    Add Linter App target

---
+++ LocMapper Linter/AppDelegate.swift
@@ -0,0 +1,24 @@
+/*
+ * AppDelegate.swift
+ * LocMapper Linter
+ *
+ * Created by François Lamboley on 12/12/2018.
+ * Copyright © 2018 happn. All rights reserved.
+ */
+
+import Cocoa
+
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+	
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+	
+}
