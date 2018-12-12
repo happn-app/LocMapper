a4b0daa427705e1c6a44b543f7e46ec0715fb157
Frizlab <fload@me.com>
2018-12-12 11:40:01 +0100

    Use XibLoc to parse XibLoc-styled string…

--- LocMapper App/Utils.swift
+++ LocMapper App/Utils.swift
@@ -9,33 +9,18 @@
 import Cocoa
 
 import LocMapper
+import XibLoc
 
 
 
 class Utils {
 	
 	static let lineKeyStrTemplate = NSLocalizedString("key description", comment: "Template for converting a LineKey object to a string.")
-	static let rangeKey: Range<String.Index>  = Utils.findRangeInString(Utils.lineKeyStrTemplate, withRegularExpression: "\\*.*\\*")
-	static let rangeFile: Range<String.Index> = Utils.findRangeInString(Utils.lineKeyStrTemplate, withRegularExpression: "\\$.*\\$")
-	static let rangeEnv: Range<String.Index>  = Utils.findRangeInString(Utils.lineKeyStrTemplate, withRegularExpression: "\\|.*\\|")
 	
 	static func lineKeyToStr(_ lineKey: LocFile.LineKey) -> String {
-		/* We assume in originalGeneralInfoText, the dynamic parts of the string
-		 * appear in the following order: env, key and file. */
-		var infoText = Utils.lineKeyStrTemplate
-		infoText.replaceSubrange(rangeFile, with: lineKey.filename)
-		infoText.replaceSubrange(rangeKey, with: lineKey.locKey)
-		infoText.replaceSubrange(rangeEnv, with: lineKey.env)
-		return infoText
-	}
-	
-	static func findRangeInString(_ string: String, withRegularExpression exprStr: String) -> Range<String.Index> {
-		let expr = try! NSRegularExpression(pattern: exprStr, options: [])
-		let range = expr.rangeOfFirstMatch(in: string, options: [], range: NSRange(location: 0, length: string.utf16.count))
-		return Range(uncheckedBounds: (
-			lower: string.index(string.startIndex, offsetBy: range.location),
-			upper: string.index(string.startIndex, offsetBy: range.location + range.length)
-		))
+		return lineKeyStrTemplate.applying(
+			xibLocInfo: Str2StrXibLocInfo(replacements: ["|": lineKey.env, "*": lineKey.locKey, "$": lineKey.filename])
+		)
 	}
 	
 	static func setTextView(_ textView: NSTextView, enabled: Bool) {
