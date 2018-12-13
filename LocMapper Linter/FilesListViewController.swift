7151e006853a11245b6bdc8e3cc94349e33c8ca1
Frizlab <fload@me.com>
2018-12-13 19:23:11 +0100

    Add possibility to add or remove files from the list

--- LocMapper Linter/FilesListViewController.swift
+++ LocMapper Linter/FilesListViewController.swift
@@ -13,11 +13,29 @@ import Cocoa
 class FilesListViewController : NSViewController, NSTableViewDataSource, NSTableViewDelegate, BecameFirstResponderTextFieldDelegate {
 	
 	@IBOutlet var tableView: NSTableView!
+	@IBOutlet var buttonAddFile: NSButton!
+	@IBOutlet var buttonRemoveFile: NSButton!
+	
+	@IBOutlet var buttonLintSelectedFile: NSButton!
+	@IBOutlet var buttonKeyVersionsCheck: NSButton!
+	
+	var filesDescription = [InputFileDescription]() {
+		didSet {
+			saveFileList()
+		}
+	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		// Do any additional setup after loading the view.
+		if let v = UserDefaults.standard.value(forKey: "FilesDescription") as? [Data] {
+			let u = v.map{ NSKeyedUnarchiver.unarchiveObject(with: $0) as? InputFileDescription }
+			let f = u.compactMap{ $0 }
+			if u.count == f.count {
+				filesDescription = f
+				refreshUI(reloadTableViewData: true)
+			}
+		}
 	}
 	
 	/* *****************************************
@@ -25,12 +43,38 @@ class FilesListViewController : NSViewController, NSTableViewDataSource, NSTable
 	   ***************************************** */
 	
 	func numberOfRows(in tableView: NSTableView) -> Int {
-		return 1
+		return filesDescription.count
 	}
 	
 	func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
 		guard let tableColumn = tableColumn else {return nil}
-		return tableView.makeView(withIdentifier: tableColumn.identifier, owner: self)
+		guard let r = tableView.makeView(withIdentifier: tableColumn.identifier, owner: self) else {return nil}
+		
+		switch tableColumn.identifier.rawValue {
+		case "nickname":
+			if let textField = r.viewWithTag(1) as? NSTextField {
+				textField.stringValue = filesDescription[row].nickname ?? "<Unnamed>"
+			}
+			
+		case "path":
+			if let textField = r.viewWithTag(1) as? NSTextField {
+				textField.stringValue = filesDescription[row].url.path
+			}
+			
+		case "git": (/*TODO*/)
+			
+		case "reflocType":
+			if let menuButton = r.viewWithTag(1) as? NSPopUpButton {
+				menuButton.selectItem(withTag: filesDescription[row].refLocType.rawValue)
+			}
+			
+		default: NSLog("Weird…")
+		}
+		return r
+	}
+	
+	func tableViewSelectionDidChange(_ notification: Notification) {
+		refreshUI(reloadTableViewData: false)
 	}
 	
 	/* ***************************
@@ -51,15 +95,70 @@ class FilesListViewController : NSViewController, NSTableViewDataSource, NSTable
       MARK: - Actions
 	   *************** */
 	
-	@IBAction func nicknameEdited(_ sender: AnyObject) {
-		guard let textView = sender as? NSTextField else {return}
-		let row = tableView.row(for: textView) /* Note: O(n)… */
-		guard row >= 0 else {return}
-		print("nicknameEdited")
+	@IBAction func addFile(_ sender: AnyObject) {
+		let openPanel = NSOpenPanel()
+		
+		openPanel.canChooseFiles = true
+		openPanel.allowedFileTypes = ["lcm"]
+		openPanel.canChooseDirectories = false
+		
+		openPanel.beginSheetModal(for: view.window!, completionHandler: { response in
+			guard response == .OK, let url = openPanel.url else {return}
+			
+			DispatchQueue.main.async{
+				self.filesDescription.append(InputFileDescription(url: url))
+				self.refreshUI(reloadTableViewData: true)
+			}
+		})
+	}
+	
+	@IBAction func removeSelectedFile(_ sender: AnyObject) {
+		let idx = tableView.selectedRow
+		guard idx >= 0 else {return}
+		
+		filesDescription.remove(at: idx)
+		self.refreshUI(reloadTableViewData: true)
+	}
+	
+	@IBAction func lintSelectedFile(_ sender: AnyObject) {
+		let idx = tableView.selectedRow
+		guard idx >= 0 else {return}
+		
+		let fileDescription = filesDescription[idx]
+		print("lint file at path \(fileDescription.url.path)")
 	}
 	
 	@IBAction func startKeyVersionsCheck(_ sender: AnyObject) {
 		print("startKeyVersionsCheck")
+	}
+	
+	@IBAction func nicknameEdited(_ sender: AnyObject) {
+		guard let textField = sender as? NSTextField else {return}
+		let row = tableView.row(for: textField) /* Note: O(n)… */
+		guard row >= 0 else {return}
+		
+		filesDescription[row].nickname = textField.stringValue
+		saveFileList()
+	}
+	
+	/* ***************
+      MARK: - Private
+	   *************** */
+	
+	private func saveFileList() {
+		let archived = filesDescription.map{
+			return NSKeyedArchiver.archivedData(withRootObject: $0)
+		}
+		UserDefaults.standard.set(archived, forKey: "FilesDescription")
+	}
+	
+	private func refreshUI(reloadTableViewData: Bool) {
+		if reloadTableViewData {tableView.reloadData()}
+		
+		let hasSelection = tableView.selectedRow >= 0
+		buttonRemoveFile.isEnabled = hasSelection
+		buttonLintSelectedFile.isEnabled = hasSelection && false /* Disabled for the time being (not implemented) */
+		buttonKeyVersionsCheck.isEnabled = !filesDescription.isEmpty
 	}
 	
 }
---
+++ LocMapper Linter/InputFileDescription.swift
@@ -0,0 +1,48 @@
+/*
+ * InputFileDescription.swift
+ * LocMapper Linter
+ *
+ * Created by François Lamboley on 13/12/2018.
+ * Copyright © 2018 happn. All rights reserved.
+ */
+
+import Foundation
+
+
+
+class InputFileDescription : NSObject, NSCoding {
+	
+	/* Raw value is tag in menu. */
+	enum RefLocType : Int {
+		
+		case xibRefLoc = 1
+		case stdRefLoc = 2
+		
+	}
+	
+	var nickname: String?
+	var url: URL
+	
+	var refLocType = RefLocType.xibRefLoc
+	
+	init(url u: URL) {
+		url = u
+		
+		super.init()
+	}
+	
+	required init?(coder aDecoder: NSCoder) {
+		url = aDecoder.decodeObject(forKey: "url") as! URL
+		nickname = aDecoder.decodeObject(forKey: "nickname") as? String
+		refLocType = RefLocType(rawValue: aDecoder.decodeInteger(forKey: "refLocType")) ?? .xibRefLoc
+		
+		super.init()
+	}
+	
+	func encode(with aCoder: NSCoder) {
+		aCoder.encode(url, forKey: "url")
+		aCoder.encode(nickname, forKey: "nickname")
+		aCoder.encode(refLocType.rawValue, forKey: "refLocType")
+	}
+	
+}
