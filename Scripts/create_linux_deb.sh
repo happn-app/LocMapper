86676ab7f7ff6cb64a98076d4df37356d1ce36ca
Frizlab <fload@me.com>
2018-12-19 17:38:14 +0100

    Enhace the create linux deb script

--- Scripts/create_linux_deb.sh
+++ Scripts/create_linux_deb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-echo "DO THIS (do not forget the VERSION variable):"
+echo "DO THIS (do not forget the VERSION variable, set to a marketing version)"
+echo "If need be, you can add a --revision N to create a new revision of the same version"
 echo 'mkdeb build --from linux_build/products/release.tar.gz --recipe Scripts/zz_assets/mkdeb locmapper:amd64=$VERSION'
