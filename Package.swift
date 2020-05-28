64685fb51f1d4868996541bbd6d5c0b07e3823e1
Frizlab <fload@me.com>
2020-05-28 14:04:38 +0200

    Start using Apple’s argument parser in LocMapper CLI

--- Package.swift
+++ Package.swift
@@ -14,7 +14,8 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/happn-tech/XibLoc.git", from: "1.0.0-rc"),
-		.package(url: "https://github.com/apple/swift-log.git", from: "1.2.0")
+		.package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.0.6"),
 	],
 	targets: [
 		.systemLibrary(name: "CZlib", path: "CZlib", providers: [.apt(["zlib1g-dev"])]),
@@ -22,13 +23,14 @@ let package = Package(
 		.target(name: "LocMapper", dependencies: ["Logging", "XibLoc", "CZlib"], path: "LocMapper"), /* A better name would be LocMapperKit. I’m lazy enough not to refactor. */
 		.testTarget(name: "LocMapperTests", dependencies: ["LocMapper"]),
 		
-		.target(name: "locmapper", dependencies: ["LocMapper"], path: "LocMapper CLI")
+		.target(name: "locmapper", dependencies: ["LocMapper", .product(name: "ArgumentParser", package: "swift-argument-parser")], path: "LocMapper CLI")
 		/* As an alternative to the two targets above, we can have only one
 		 * “locmapper” target that compile both folders directly. I prefer the
 		 * lib/executable structure (among other it allows having other targets
-		 * that uses the LocMapper lib), but here’s the alternative (note it does
-		 * not work out of the box because the FileHandleOutputStream class is
-		 * implemented both in the lib and the executable…):
-		.target(name: "locmapper", dependencies: ["XibLoc"], path: ".", sources: ["LocMapper", "LocMapper CLI"])*/
+		 * that uses the LocMapper lib, like the test target for instance), but
+		 * here’s the alternative (note it does not work out of the box because
+		 * the FileHandleOutputStream class is implemented both in the lib and the
+		 * executable…):
+		.target(name: "locmapper", dependencies: ["XibLoc", .product(name: "ArgumentParser", package: "swift-argument-parser")], path: ".", sources: ["LocMapper", "LocMapper CLI"])*/
 	]
 )
